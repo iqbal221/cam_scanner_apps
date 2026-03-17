ab2753ddc2e7f894a65e0f4443a1372ea8798890
MD ZAHED IQBAL <tajiniiuc222@gmail.com>
2026-03-17 21:33:33 +0600

    home screen started

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -10,6 +10,46 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color(0xFF303136),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF303136),
+        elevation: 3,
+        title: Text("Scanner", style: TextStyle(color: Colors.white)),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.delete, color: Colors.red),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.document_scanner, size: 80, color: Colors.white),
+            SizedBox(height: 20),
+            Text(
+              "No Documents Yet",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Tap the camera button to scan",
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color(0xFF29A8FF),
+        onPressed: () {},
+        child: Icon(Icons.camera_alt, size: 28),
+      ),
+    );
   }
 }
